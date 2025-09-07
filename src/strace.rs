7d2b867da8340a31e9b76d9de5e35bbb44338ddb
Kyle Lacy <kylelacy@kyle.space>
2025-09-06 23:14:28 -0700

    Add initial `EventEmitter` from strace lines

--- src/strace.rs
+++ src/strace.rs
@@ -1,5 +1,6 @@
 use std::borrow::Cow;
 
+pub mod emitter;
 pub mod parser;
 
 #[derive(Debug)]
@@ -62,10 +63,76 @@ enum Value {
     Truncated,
 }
 
+impl Value {
+    fn to_bstring(&self) -> Option<Cow<'_, bstr::BStr>> {
+        match self {
+            Self::String(bstring) => Some(Cow::Borrowed(bstr::BStr::new(bstring))),
+            Self::TruncatedString(bstring) => {
+                let mut bstring = bstring.clone();
+                bstring.extend_from_slice(b"...");
+                Some(Cow::Owned(bstring))
+            }
+            Self::Expression(_) => None,
+            Self::FunctionCall { .. } => None,
+            Self::Struct(..) => None,
+            Self::SparseArray(..) => None,
+            Self::Array(..) => None,
+            Self::NotBitSet(..) => None,
+            Self::Annotated {
+                value,
+                annotation,
+                deleted: _,
+            } => {
+                let bstring = value
+                    .to_bstring()
+                    .unwrap_or(Cow::Borrowed(bstr::BStr::new(annotation)));
+                Some(bstring)
+            }
+            Self::Commented { value, comment: _ } => value.to_bstring(),
+            Self::Changed { from, to: _ } => from.to_bstring(),
+            Self::Alternative { left, right: _ } => left.to_bstring(),
+            Self::Truncated => None,
+        }
+    }
+
+    fn as_array(&self) -> Option<&[Value]> {
+        if let Self::Array(values) = self {
+            Some(values)
+        } else {
+            None
+        }
+    }
+
+    fn as_i32(&self) -> Option<i32> {
+        if let Self::Expression(expr) = self {
+            expr.parse().ok()
+        } else {
+            None
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Fields {
     entries: Vec<(Option<String>, Value)>,
     truncated: bool,
+}
+
+impl Fields {
+    fn value_at_index(&self, index: usize) -> Option<&Value> {
+        if let Some((_, value)) = self.entries.get(index) {
+            return Some(value);
+        }
+
+        if self.truncated {
+            None
+        } else {
+            panic!(
+                "field index {index} is out of bounds (number of fields: {})",
+                self.entries.len()
+            );
+        }
+    }
 }
 
 #[derive(Debug)]
---
+++ src/strace/emitter.rs
@@ -0,0 +1,116 @@
+use std::{
+    borrow::Cow,
+    collections::{HashSet, VecDeque},
+};
+
+use bstr::ByteSlice;
+
+use crate::event::{Event, EventKind, ProcessExec, StopProcessEvent};
+
+#[derive(Default)]
+pub struct EventEmitter {
+    alive_processes: HashSet<libc::pid_t>,
+    events: VecDeque<Event>,
+}
+
+impl EventEmitter {
+    pub fn push_line(&mut self, line: super::Line) {
+        let timestamp = line.timestamp;
+        let pid = line.pid;
+
+        let is_new_process = self.alive_processes.insert(line.pid);
+        if is_new_process {
+            self.events.push_back(Event {
+                timestamp,
+                pid,
+                kind: EventKind::StartProcess,
+            });
+        }
+
+        match line.event {
+            super::Event::Syscall {
+                name,
+                args,
+                result: _,
+                duration: _,
+            } => match name.as_str() {
+                "execve" => {
+                    let command = args
+                        .value_at_index(0)
+                        .and_then(super::Value::to_bstring)
+                        .map(Cow::into_owned);
+                    let exec_args =
+                        args.value_at_index(1)
+                            .and_then(super::Value::as_array)
+                            .map(|args| {
+                                args.iter()
+                                    .map(|arg| {
+                                        arg.to_bstring()
+                                            .unwrap_or(Cow::Borrowed(bstr::BStr::new(
+                                                b"<unknown arg>",
+                                            )))
+                                            .into_owned()
+                                    })
+                                    .collect()
+                            });
+                    let env = args
+                        .value_at_index(2)
+                        .and_then(super::Value::as_array)
+                        .map(|env| {
+                            env.iter()
+                                .filter_map(|env| {
+                                    let env = env.to_bstring()?;
+                                    let (key, value) = env.split_once_str(b"=")?;
+                                    Some((bstr::BString::from(key), bstr::BString::from(value)))
+                                })
+                                .collect()
+                        });
+
+                    self.events.push_back(Event {
+                        timestamp,
+                        pid,
+                        kind: EventKind::ExecProcess(ProcessExec {
+                            command,
+                            args: exec_args,
+                            env,
+                        }),
+                    });
+                }
+                _ => {}
+            },
+            super::Event::Signal { .. } => {}
+            super::Event::Exited { code } => {
+                let did_remove = self.alive_processes.remove(&pid);
+                if did_remove {
+                    self.events.push_back(Event {
+                        timestamp,
+                        pid,
+                        kind: EventKind::StopProcess(StopProcessEvent::Exited {
+                            code: code.as_i32(),
+                        }),
+                    });
+                }
+            }
+            super::Event::KilledBy { signal } => {
+                let did_remove = self.alive_processes.remove(&pid);
+                if did_remove {
+                    let signal = if let super::Value::Expression(signal) = signal {
+                        Some(signal)
+                    } else {
+                        None
+                    };
+
+                    self.events.push_back(Event {
+                        timestamp,
+                        pid,
+                        kind: EventKind::StopProcess(StopProcessEvent::Killed { signal }),
+                    });
+                }
+            }
+        }
+    }
+
+    pub fn pop_event(&mut self) -> Option<Event> {
+        self.events.pop_front()
+    }
+}
